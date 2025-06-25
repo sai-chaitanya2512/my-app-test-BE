3c4f51b5ab91ae93861d24e31031dbb45efe9eff
sai-chaitanya2512 <saichaitanya5191@gmail.com>
2025-06-26 02:35:10 +0530

    added logout button

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,8 +1,21 @@
+import { Button } from 'antd';
 import React from 'react'
 
 const Dashboard = () => {
+
+    const navigate = useNavigate();
+
+    const logOut = () => {
+        localStorage.removeItem("auth-token");
+        navigate("/login");
+    }
+
   return (
-    <div>Dashboard</div>
+    <div>
+        <h1>Dashboard</h1>
+        {/* logout button */}
+        <Button onClick={logOut}>Logout</Button>
+    </div>
   )
 }
 
